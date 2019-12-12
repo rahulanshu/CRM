113c942fa905c0f35edb36717f5e35144ed1abad
rahulanshu <48986355+rahulanshu@users.noreply.github.com>
2019-12-12 21:25:56 +0530

    Add files via upload

---
+++ about.php
@@ -0,0 +1,74 @@
+<!doctype html>
+<html class="no-js" lang="en-US">
+<meta http-equiv="content-type" content="text/html;charset=utf-8" />
+<head>
+ <!-- DEFAULT META TAGS -->
+    <?php include 'include/top_css_js.php';?>
+    <!-- DOCUMENT TITLE -->
+    <title>Rao | About</title>
+</head>
+<body>
+
+
+<!-- PAGE CONTENT -->
+<div id="page-content">
+
+	<!-- HEADER -->
+	 	<?php include 'include/header.php';?>
+	    
+    <!-- HERO  -->
+	<section id="hero" class="hero-auto text-light parallax-section">
+    	
+        <div id="page-title" class="wrapper align-center">
+            <h1><strong>Our Story</strong></h1>
+                 </div> <!-- END #page-title -->
+            
+    </section>
+    <!-- HERO -->
+        <!-- PAGEBODY -->
+	
+    <section id="page-body1">
+    
+    	<div class="column-section boxed-sticky adapt-height clearfix">
+        	<div class="column one-half bigpadding" style="background: transparent">
+            	<h4 class="uppercase"><strong>Our Story</strong></h4>
+                <hr class="small thick sm">
+                 <p class="prr">It all started over 25 years ago, with a tiny VCR rental store. We've come a long way since then and multiplied our product line by manifold.</p>
+                <p class="prr">We are leaders in the world of events who are committed towards making your vision a technicolour reality.</p>
+				<p class="prr">We are able to do this through a wide array of AV products including various display screens and sound systems.</p>
+				<p class="prr">With our experience, we can guarantee the success of the event.</p>
+				<p class="prr">It is our constant endeavour to provide excellent products and unrivalled support to best serve our clients. We have raised the bar with every show
+				in terms of technology, management and innovation,constantly setting new standards of excellence..</p>
+              </div>
+            <div class="column one-half last-col" style="background:url(files/uploads/about.jpeg) center center;background-size:cover;">
+            	
+            </div>
+        </div>
+        
+        <div class="spacer-big"></div>
+        
+        <div class="column-section boxed-sticky adapt-height clearfix">
+            <div class="column one-half" style="background:url(files/uploads/about1.jpg) center center;background-size:cover;"></div>
+            <div class="column one-half last-col bigpadding" style="background: transparent">
+            	<h4 class="uppercase"><strong>What we do</strong></h4>
+                <hr class="small thick sm">
+                <p class="prr">With much of our modern communication done visually,prioritising visual and audio communications is a smart way to gain traction among patrons.</p>
+      <p class="prr">We offer a complete and diverse selection of AV products including display screens and sound systems.Regardless of your audience size, we can help you optimally deliver the message.</p>
+		</div>
+        </div>
+        <div class="spacer-medium"></div>
+      
+ 	</section>
+    
+    <!-- FOOTER -->  
+     <?php include 'include/footer.php';?>
+    <!-- FOOTER -->
+    
+</div> <!-- END #page-content -->
+<!-- PAGE CONTENT -->
+
+<!-- SCRIPTS -->
+<?php include 'include/bottom_css_js_inner.php';?>
+
+</body>
+</html>
